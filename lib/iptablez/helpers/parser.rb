5a0fc15ad0697339e6c21ed172ea036d09bfca1e
Kent Gruber <kgruber1@emich.edu>
2017-04-29 17:10:34 -0400

    More parsing fun! 👀🍪With dem defaults too.

--- lib/iptablez/helpers/parser.rb
+++ lib/iptablez/helpers/parser.rb
@@ -5,7 +5,7 @@ module Iptablez
     # @todo Document better
     # iptables -L
     # iptables -L -v
-    def self.list_to_array(output)
+    def self.list_to_array(output = `iptables -L`)
       results = []
       output.split("\n").each do |line|
         next if line.match(/\bChain\s\w+\s\(\w+\s+\w+(\s+\w+\spackets,\s\w+\sbytes)?\)/) 
@@ -20,7 +20,7 @@ module Iptablez
     # @todo Document better
     # iptables -L
     # iptables -L -v
-    def self.list_to_chains(output)
+    def self.list_to_chains(output = `iptables -L`)
       results = []
       output.split("\n").each do |line|
         next unless line.match(/\bChain\s\w+/)
@@ -30,6 +30,37 @@ module Iptablez
       end
       results
     end    
+
+    def self.list_rules_to_array(output = `iptables -S`)
+      results = []
+      output.split("\n").each do |line|  
+        next line if line.empty?
+        yield line if block_given?
+        results << line
+      end
+      results
+    end
+
+    def self.list_rules_to_policies(output = `iptables -S`)
+      results = {}
+      output.split("\n").each do |line|
+        next unless line.match(/-P\s/)
+        chain, rule = line.split[1,2]
+        yield [chain, rule] if block_given?
+        results[chain] = rule
+      end
+      results
+    end
+
+    def self.list_rules_to_policy(policy, output = `iptables -S`)
+      output.split("\n").each do |line|
+        next unless line.match(/-P\s#{policy}/)
+        chain, rule = line.split[1,2]
+        yield [chain, rule] if block_given?
+        return { chain => rule }
+      end
+      return false
+    end
 
   end
 
